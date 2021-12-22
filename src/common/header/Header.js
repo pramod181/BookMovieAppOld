26ad3953eba85419d1db81d8a4960a3609a8e86e
unknown <promadg@gmail.com>
2021-12-22 21:27:09 +0530

    Book show button updated

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ReactDOM } from "react";
 import './Header.css';
 import companyLogo from '../../assets/logo.svg';
 import Button from "@material-ui/core/Button";
 
-function openModelHandler(){
+function openModelHandler() {
     return null;
 }
 
 
 const Header = function (props) {
+    const [isLoggedIn, setIsLoggedIn] = useState(props.isLoggedIn);
 
-    let isLoggedIn = props.isLoggedIn;
     let loginBtnDisplay, logoutBtnDisplay;
     if (isLoggedIn) {
         loginBtnDisplay = { display: "none", margin: "2px 10px" };
@@ -20,13 +20,32 @@ const Header = function (props) {
         loginBtnDisplay = { display: "inline" };
         logoutBtnDisplay = { display: "none" };
     }
-    let enableBooking = props.enableBooking;
-    let bookBtnDisplay;
+    var enableBooking = props.enableBooking;
+
+    var bookGuestBtnDisplay = { display: "none", margin: "2px 10px" };
+    var bookUserBtnDisplay = { display: "none", margin: "2px 10px" };
+
     if (enableBooking) {
-        bookBtnDisplay = { display: "inline", margin: "2px 10px" };
-    } else {
-        bookBtnDisplay = { display: "none", margin: "2px 10px" };
+        if (isLoggedIn) {
+            bookUserBtnDisplay = { display: "inline", margin: "2px 10px" };
+        } else {
+            bookGuestBtnDisplay = { display: "inline", margin: "2px 10px" };
+        }
     }
+
+
+
+    useEffect(() => {
+
+        if (enableBooking) {
+            if (isLoggedIn) {
+                bookUserBtnDisplay = { display: "inline", margin: "2px 10px" };
+            } else {
+                bookGuestBtnDisplay = { display: "inline", margin: "2px 10px" };
+            }
+        }
+
+    }, [isLoggedIn])
 
     return (
         <div className="header">
@@ -34,7 +53,10 @@ const Header = function (props) {
                 <img className="logo" src={companyLogo} alt="companyLogo" ></img>
             </div>
             <div className="btn">
-                <Button className="bookShowBtn" variant="contained" color="primary" size="small" style={bookBtnDisplay}>
+                <Button className="bookShowBtn" variant="contained" color="primary" size="small" style={bookGuestBtnDisplay} onClick={bookGuestHandler}>
+                    Book Show
+                </Button>
+                <Button className="bookShowBtn" variant="contained" color="primary" size="small" style={bookUserBtnDisplay} onClick={book}>
                     Book Show
                 </Button>
                 <Button variant="contained" color="default" size="small" style={logoutBtnDisplay} onClick={openModelHandler}>
